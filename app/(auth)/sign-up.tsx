daf3dc5d2b8f5e0bdffb9a1c17ff7de2339eddae
jiyu13 <jiyucoding@yahoo.com>
2025-01-06 14:03:46 -0500

    create auth + root routes

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+
+export default function SignUp() {
+    return (
+        <SafeAreaView className="flex-1 items-center justify-center bg-white">
+            <Text className="text-center">Sign Up</Text>
+        </SafeAreaView>
+    )
+}
