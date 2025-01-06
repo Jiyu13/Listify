50a6ae13298843b405b4c78b2d632f2df656deba
jiyu13 <jiyucoding@yahoo.com>
2025-01-06 13:51:16 -0500

    redirect to welcome page

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
 import {View, Text} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
+import {Redirect} from "expo-router";
 
 export default function Home() {
     return (
-        <SafeAreaView className="flex-1 items-center justify-center bg-white">
-            <Text className="text-center">Home</Text>
-        </SafeAreaView>
+        // <SafeAreaView className="flex-1 items-center justify-center bg-white">
+        //     <Text className="text-center">Home</Text>
+        // </SafeAreaView>
+
+        <Redirect href="./(auth)/welcome"/>
     )
 }
