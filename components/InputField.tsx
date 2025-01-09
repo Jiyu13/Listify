ad394a3ec9b2761235cf69ea5d51f04c173eadc7
jiyu13 <jiyucoding@yahoo.com>
2025-01-08 20:00:48 -0500

    create a custom input field component which accepts InputFieldProps

---
+++ components/InputField.tsx
@@ -0,0 +1,33 @@
+import {Keyboard, KeyboardAvoidingView, Platform, Text, TextInput, TouchableWithoutFeedback, View} from "react-native";
+import {InputFieldProps} from "@/types/type";
+
+export default function InputField({
+       label, name,
+       labelStyle,
+       secureTextEntry=false,
+       containerStyle,
+       inputStyle,
+       className,
+        ...props
+    }: InputFieldProps) {
+    return (
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View className="my-2 w-full">
+                    <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle} text-primary-900`}>
+                        {label}
+                    </Text>
+                    <View className={`flex flex-row justify-start items-center relative bg-neutral-100 border border-neutral-100 focus:border-primary-500 ${containerStyle}`}>
+                        <TextInput
+                            className={`p-4 font-JakartaSemiBold text-[15px] flex-1 ${inputStyle} text-left`}
+                            secureTextEntry={secureTextEntry}
+                            {...props}
+                        >
+
+                        </TextInput>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+    )
+}
