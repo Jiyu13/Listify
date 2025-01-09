38a90044a745a4825f11a7b149f81aebbd1aa850
jiyu13 <jiyucoding@yahoo.com>
2025-01-08 23:12:33 -0500

    access the user's authentication state with useAuth + check if user is logged in

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,14 @@ import {View, Text, StatusBar} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
 import {Redirect} from "expo-router";
 import "../global.css"
+import {useAuth} from "@clerk/clerk-expo";
 
 export default function Home() {
+    const { isSignedIn } = useAuth()
+
+    if (isSignedIn) {
+        return  <Redirect href="/(root)/(tabs)/home"/>
+    }
     return (
         // <SafeAreaView>
         //     <StatusBar hidden={false} />
