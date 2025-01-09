84797e10ea9d27c9bc79f9d2dfe8119ce7c2d630
jiyu13 <jiyucoding@yahoo.com>
2025-01-08 23:17:05 -0500

    integrate Clerk Expo SDK for authentication + display different UI elements based on whether user is signed in / signed out

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,10 +1,24 @@
 import {View, Text} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
+import {SignedIn, SignedOut, useUser} from "@clerk/clerk-expo";
+import {Link} from "expo-router";
 
-export default function Home() {
+export default function Page() {
+    const {user} = useUser()
+
     return (
-        <SafeAreaView className="flex-1 items-center justify-center bg-white">
-            <Text className="text-center">Home</Text>
-        </SafeAreaView>
+        <View>
+            <SignedIn>
+                <Text>hello {user?.emailAddresses[0].emailAddress}</Text>
+            </SignedIn>
+            <SignedOut>
+                <Link href="../../(auth)/sign-in">
+                    <Text>Sign In</Text>
+                </Link>
+                <Link href="../../(auth)/sign-in">
+                    <Text>Sign Up</Text>
+                </Link>
+            </SignedOut>
+
+        </View>
     )
 }
