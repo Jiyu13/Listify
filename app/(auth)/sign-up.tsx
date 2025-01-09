f0597c1ad48412743a3fb136d80a05d24b4df8e4
jiyu13 <jiyucoding@yahoo.com>
2025-01-08 23:19:54 -0500

    use useSignUp hook to  a signup flow + edit onSignUpPress function + verify press

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,13 +1,18 @@
 import {View, Text, ScrollView} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
 import InputField from "@/components/InputField";
-import {useState} from "react";
+import React, {useState} from "react";
 import CustomButton from "@/components/CustomButton";
-import { Link, router } from "expo-router";
+import {Link, router, useRouter} from "expo-router";
 import OAuth from "@/components/OAuth";
+import {useSignUp} from "@clerk/clerk-expo";
 
 export default function SignIn() {
 
+    const { isLoaded, signUp, setActive } = useSignUp()
+
+    // ==========================Verification - user login /signup by email==========================
+    const [verification, setVerification] = useState({state: "", error: "", code: ""})
     const [formData, setFormData] = useState({email: "", username: "", password: ""})
 
     function handleInput(name:string, value:string) {
@@ -16,8 +21,77 @@ export default function SignIn() {
         setFormData((prevFormData) => ({...prevFormData, [name]: value}))
     }
 
-    function onSignUpPress() {
-        return
+    const onSignUpPress = async () => {
+        if (!isLoaded) return
+
+        // Start sign-up process using email and password provided
+        try {
+            await signUp.create({
+                emailAddress: formData.email,
+                password: formData.password,
+            })
+
+            // Send user an email with verification code
+            await signUp.prepareEmailAddressVerification({ strategy: 'email_code' })
+
+            // Set 'pendingVerification' to true to display second form
+            // and capture OTP code
+            setVerification((prevVerification) => ({...prevVerification, state: "pending"}))
+        } catch (err) {
+            console.error(JSON.stringify(err, null, 2))
+        }
+    }
+
+    // Handle submission of verification form
+    // ==========================Email with code to verify whether it is us who trys to sign up with email==========================
+    const onVerifyPress = async () => {
+        if (!isLoaded) return
+
+        try {
+            // Use the code the user provided to attempt verification
+            const signUpAttempt = await signUp.attemptEmailAddressVerification({
+                code: verification.code,
+            })
+
+            // If verification was completed, set the session to active
+            // and redirect the user
+            if (signUpAttempt.status === 'complete') {
+                // =======================create a new user once complete=======================
+                // TODO: Create a database user!
+
+                await setActive({ session: signUpAttempt.createdSessionId })
+                // =======================also modify the verification state==========================
+                setVerification((prevVerification) => ({...prevVerification, state: "success"}))
+                router.replace('/')
+            } else {
+                setVerification((prevVerification) => ({
+                    ...prevVerification,
+                    state: "failed", error: "Verification failed."
+                }))
+                // // If the status is not complete, check why. User may need to
+                // // complete further steps.
+                // console.error(JSON.stringify(signUpAttempt, null, 2))
+            }
+        } catch (err: any) {
+            setVerification((prevVerification) => ({
+                ...prevVerification,
+                state: "failed", error: err.error[0].longMessage
+            }))
+        }
+    }
+
+    if (pendingVerification) {
+        return (
+            <>
+                <Text>Verify your email</Text>
+                <TextInput
+                    value={code}
+                    placeholder="Enter your verification code"
+                    onChangeText={(code)=> setCode(code)}
+                />
+                <Button title="Verify" onPress={onVerifyPress} />
+            </>
+        )
     }
 
     return (
