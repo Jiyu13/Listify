02f044527d823745d81642bb8072de4658d2ff68
jiyu13 <jiyucoding@yahoo.com>
2025-01-10 18:50:10 -0500

    declare a StackScreen for (tabs) folder -

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,3 +1,15 @@
+import { Stack } from 'expo-router';
+import {StatusBar} from "expo-status-bar";
+
 export default function RootLayout() {
-    return
+    return (
+        <>
+            <StatusBar style='dark' />
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
+        </>
+
+    );
 }
+
