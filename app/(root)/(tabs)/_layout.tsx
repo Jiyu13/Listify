bf08feee29d16599149b26aa3a3f733a417359d3
jiyu13 <jiyucoding@yahoo.com>
2025-01-10 22:42:31 -0500

    define a Tabs navigator + set up icons + custmize Tab options + render tabs layout

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,6 +1,61 @@
 import {Stack, Tabs} from 'expo-router';
+import {Image, View} from "react-native";
+
+function TabIcon() {
+    return (
+        <View>
+            <View>
+                <Image source={"/"}/>
+            </View>
+        </View>
+    )
+
+}
 
 export default function RootTabsLayout() {
 
-    return <Tabs />;
+    return (
+        <Tabs
+            initialRouteName="index"
+            screenOptions={{
+                tabBarActiveTintColor: "white",
+            }}
+        >
+            <Tabs.Screen
+                name="home"
+                options={{
+                    title: "Home",
+                    headerShown: false,
+                    tabBarIcon: () => <TabIcon />
+                }}
+            />
+            <Tabs.Screen
+                name="rides"
+                options={{
+                    title: "Rides",
+                    headerShown: false,
+                    tabBarIcon: () => <TabIcon />
+
+                }}
+            />
+            <Tabs.Screen
+                name="chat"
+                options={{
+                    title: "Chat",
+                    headerShown: false,
+                    tabBarIcon: () => <TabIcon />
+
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: "Profile",
+                    headerShown: false,
+                    tabBarIcon: () => <TabIcon  />
+
+                }}
+            />
+        </Tabs>
+    )
 }
