9469ec0aa8f5243ae668bd7c54bd45083d72a56f
jiyu13 <jiyucoding@yahoo.com>
2025-01-11 16:29:15 -0500

    add tab bar style for <Tabs> + custiomize TabIcon()

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,11 +1,22 @@
 import {Stack, Tabs} from 'expo-router';
-import {Image, View} from "react-native";
+import {Image, ImageSourcePropType, View} from "react-native";
+import {icons} from "@/constants"
 
-function TabIcon() {
+
+function TabIcon({source, focused}: { source: ImageSourcePropType, focused: boolean }) {
     return (
-        <View>
-            <View>
-                <Image source={"/"}/>
+        <View
+            className={`flex justify-center items-center ${focused ? "bg-general-300" : ""}`}  // white
+        >
+            <View
+                className={`w-12 h-12 items-center justify-center ${focused ? "bg-general-400" : ""}`} // green
+            >
+                <Image
+                    source={source}
+                    tintColor="white"
+                    resizeMode="contain"
+                    className="w-7 h-7"
+                />
             </View>
         </View>
     )
@@ -16,9 +27,19 @@ export default function RootTabsLayout() {
 
     return (
         <Tabs
-            initialRouteName="index"
+            initialRouteName="home"
             screenOptions={{
                 tabBarActiveTintColor: "white",
+                tabBarInactiveTintColor: "white",
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    backgroundColor: "#333333",
+                    height: 68,
+                    overflow: "hidden",
+                    justifyContent: 'space-around',
+                    alignItems: "center",
+                    paddingVertical: 0
+                },
             }}
         >
             <Tabs.Screen
@@ -26,24 +47,9 @@ export default function RootTabsLayout() {
                 options={{
                     title: "Home",
                     headerShown: false,
-                    tabBarIcon: () => <TabIcon />
-                }}
-            />
-            <Tabs.Screen
-                name="rides"
-                options={{
-                    title: "Rides",
-                    headerShown: false,
-                    tabBarIcon: () => <TabIcon />
-
-                }}
-            />
-            <Tabs.Screen
-                name="chat"
-                options={{
-                    title: "Chat",
-                    headerShown: false,
-                    tabBarIcon: () => <TabIcon />
+                    tabBarIcon: ({focused, }) => (
+                        <TabIcon focused={focused} source={icons.list}/>
+                    )
 
                 }}
             />
@@ -52,10 +58,12 @@ export default function RootTabsLayout() {
                 options={{
                     title: "Profile",
                     headerShown: false,
-                    tabBarIcon: () => <TabIcon  />
-
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon focused={focused} source={icons.profile}/>
+                    )
                 }}
             />
+
         </Tabs>
     )
 }
