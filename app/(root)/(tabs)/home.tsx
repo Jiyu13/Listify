e79348a6294f04dcfd26739d6bd284e0b1f56663
jiyu13 <jiyucoding@yahoo.com>
2025-01-12 13:19:35 -0500

    test making get request from backend

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,12 +1,29 @@
 import {View, Text} from "react-native";
 import {SignedIn, SignedOut, useUser} from "@clerk/clerk-expo";
 import {Link} from "expo-router";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function Page() {
     const {user} = useUser()
+    const [testMessage, setTestMessage] = useState('');
+
+    console.log("Home Page Loaded");
+    console.log(user?.emailAddresses[0].emailAddress)
+
+    useEffect(() => {
+        axios('http://192.168.1.168:5000/api/v1/listify')
+            .then(res => {
+                setTestMessage(res.data.message)
+            })
+            .catch(error => {
+                console.error("Error fetching data", error)}
+            )
+    }, [])
 
     return (
-        <View>
+        <SafeAreaView>
             <SignedIn>
                 <Text>hello {user?.emailAddresses[0].emailAddress}</Text>
             </SignedIn>
@@ -18,7 +35,6 @@ export default function Page() {
                     <Text>Sign Up</Text>
                 </Link>
             </SignedOut>
-
-        </View>
+        </SafeAreaView>
     )
 }
