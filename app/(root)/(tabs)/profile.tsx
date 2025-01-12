b48f49b282b70e5babf4c8471c7c25e28248bc1c
jiyu13 <jiyucoding@yahoo.com>
2025-01-12 13:15:19 -0500

    return text content

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,3 +1,12 @@
+import {SignedIn, SignedOut} from "@clerk/clerk-expo";
+import {Text} from "react-native";
+import {Link} from "expo-router";
+import {SafeAreaView} from "react-native-safe-area-context";
+
 export default function RootProfile() {
-    return
+    return (
+        <SafeAreaView>
+            <Text>Profile</Text>
+        </SafeAreaView>
+    )
 }
