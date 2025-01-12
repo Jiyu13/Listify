721c07688f9f55c48a37e0a4f9a25c71d1afed9e
jiyu13 <jiyucoding@yahoo.com>
2025-01-12 16:36:44 -0500

    add postgres pool

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const { Pool } = require('pg')
+require('dotenv').config()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// ==========================PostgreSQL pool=========================================================
+const pool = new Pool ({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+})
+pool.connect().then(() => console.log("connected"))
+
+// ======================middleware=================================================================
 app.use(cors())
 app.use(express.json())
 
