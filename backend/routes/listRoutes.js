35ea7e79c69cb8d67d0f797d86918781b5d14017
jiyu13 <jiyucoding@yahoo.com>
2025-01-13 22:34:35 -0500

    define and export route related to users table

--- backend/routes/listRoutes.js
+++ backend/routes/listRoutes.js
@@ -1,7 +1,40 @@
 const express = require('express')
+const pool = require("../db/db");
 const router = express.Router()
 
-// define routes related to list functionality
+// define routes related to lists table
+router.get('/', async (req, res) => {
+    try {
+        const allLists = await pool.query(
+            'SELECT * from lists'
+        )
+        res.json(allLists.rows)
+        console.log()
+    }catch (error) {
+        console.error(error.message)
+        res.status(500).json({ error: "Server error" });
+    }
+})
 
+router.post('/', async (req, res) => {
+    try {
+        // console.log(res.req.body)
+        const {name} = res.req.body
+        const newList = await pool.query(
+            "INSERT INTO lists (name) VALUES ($1) RETURNING *", [name]
+        )
+        res.json(newList)
+    }catch (error) {
+        console.error(error.message)
+        res.status(500).json({ error: "Server error" });
+    }
+})
 
+// router.get('/:id', (req, res) => {
+//   const list = lists.find(l => l.id === parseInt(req.params.id));
+//   if (!list) {
+//     return res.status(404).send('List not found');
+//   }
+//   res.json(list);
+// });
 module.exports = router;
