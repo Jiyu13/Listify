05f6642d4fb0941122a56f6a94566a20ceea225a
jiyu13 <jiyucoding@yahoo.com>
2025-01-13 22:35:51 -0500

    import and use routes

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,44 @@
 const express = require('express');
 const cors = require('cors');
-const { Pool } = require('pg')
 require('dotenv').config()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+// const pool = require('./db/db')
 
-// ==========================PostgreSQL pool=========================================================
-const pool = new Pool ({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-})
-pool.connect().then(() => console.log("connected"))
+// IMPORT ROUTE MODULES
+const listRouter = require('./routes/listRoutes')
+const userRouter = require('./routes/userRoutes')
 
-// ======================middleware=================================================================
+// MIDDLEWARES
 app.use(cors())
 app.use(express.json())
 
-// ============================Define your API routes here ===============================================
+
+
+// USER ROUTES -
+// don't put a dot at the front of the string route
+app.use('/api/v1/listify/lists', listRouter)
+app.use('/api/v1/listify/users', userRouter)
+
+
+// DEFAULT ROUTE
 app.get('/api/v1/listify', (req, res) => {
     const data = { message: 'Hello from the API!' };
     res.json(data);
+    console.log(data)
 })
-// ============================================================================================================
+
+// Test if connected successfully
+// pool.query('SELECT * from lists', (err, res) => {
+//     if (err) {
+//         console.error('Error connecting to database:', err);
+//     } else {
+//         console.log('Database connection successful:', res.rows);
+//     }
+// });
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
