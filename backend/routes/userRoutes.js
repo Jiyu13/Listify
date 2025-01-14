6f5cbcd05a23cd95eae108c5bbbbc04f219a3364
jiyu13 <jiyucoding@yahoo.com>
2025-01-13 22:35:11 -0500

    define and export route related to users table

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,24 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const pool = require("../db/db");
+
 const router = express.Router()
 
+// const hashPassword = async (password) => {
+//     const saltRounds = 10;
+//     const hashedPassword = await bcrypt.hash(password, saltRounds)
+//     return hashedPassword
+// }
+router.post('/', async (req, res) => {
+    try {
+        const {username, email, password} = res.req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const query = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3)';
+        const newUser = await pool.query(query, [username, email, hashedPassword])
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({ error: "Server error" });
+    }
+})
 
 module.exports = router;
