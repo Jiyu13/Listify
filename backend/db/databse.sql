db887b01d1d2de20b1da83ac290e542e806285df
jiyu13 <jiyucoding@yahoo.com>
2025-01-15 12:34:53 -0500

    create junction table users_lsits + select lists associated with target user id

--- backend/db/databse.sql
+++ backend/db/databse.sql
@@ -31,16 +31,17 @@ ALTER TABLE lists RENAME COLUMN id TO list_id;
 ALTER TABLE users RENAME COLUMN user_id TO id;
 ALTER TABLE lists RENAME COLUMN list_id TO id;
 
--- USER_LISTS
+-- USER_LISTS JUNCTION TABLE
 CREATE TABLE users_lists (
     user_id INT REFERENCES users(id) ON DELETE CASCADE, -- References the `users` table
     list_id INT REFERENCES lists(id) ON DELETE CASCADE, -- References the `lists` table
     PRIMARY KEY (user_id, list_id)                     -- Composite primary key to ensure uniqueness
 );
 INSERT INTO users_lists (user_id, list_id) VALUES (16, 1), (16, 2), (16, 3), (16, 4);
---
-SELECT l.* FROM lists l                 -- select all columns from teh lists table, l is its alias
-    JOIN users_lists ul                 -- joins the lists table with the users_lists table where the list_id matches
-        ON l.id = ul.list_id            -- only rows where list_id in both tables match are includes
-           WHERE ul.user_id = 16        -- filter the results to include only rows where user_id is 16
-           ORDER BY l.created_at DESC;  -- order the lists
+
+-- select lists associated with a specific user id
+SELECT l.* FROM lists l      -- select all columns from teh lists table, l is its alias
+JOIN users_lists ul          -- joins the lists table with the users_lists table where the list_id matches
+ON l.id = ul.list_id         -- only rows where list_id in both tables match are includes
+WHERE ul.user_id = 16        -- filter the results to include only rows where user_id is 16
+ORDER BY l.created_at DESC;  -- order the lists
