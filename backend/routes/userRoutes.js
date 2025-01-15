7a3e05e9f9c31d8b376a3722fc8280e503dfd220
jiyu13 <jiyucoding@yahoo.com>
2025-01-14 19:08:37 -0500

    create function to hash plain password + correctly destructure request body

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,17 +4,21 @@ const pool = require("../db/db");
 
 const router = express.Router()
 
-// const hashPassword = async (password) => {
-//     const saltRounds = 10;
-//     const hashedPassword = await bcrypt.hash(password, saltRounds)
-//     return hashedPassword
-// }
+const hashPassword = async (password) => {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    return await bcrypt.hash(password, salt)
+}
 router.post('/', async (req, res) => {
     try {
-        const {username, email, password} = res.req.body
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const query = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3)';
-        const newUser = await pool.query(query, [username, email, hashedPassword])
+        const {newUser} = req.body
+        const hashedPassword = await hashPassword(newUser.password)
+        const query = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *';
+        const data = await pool.query(query, [newUser.username, newUser.email, hashedPassword])
+        res.status(201).json({
+            message: "User created successfully",
+            data: data.rows[0]
+        })
     } catch (error) {
         console.error(error.message)
         res.status(500).json({ error: "Server error" });
