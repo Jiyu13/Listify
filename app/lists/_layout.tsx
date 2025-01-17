16e852f05cd5840a3a0933e3adb9bb5de76947b3
jiyu13 <jiyucoding@yahoo.com>
2025-01-17 10:12:52 -0500

    define a header for all screens in /lists route

---
+++ app/lists/_layout.tsx
@@ -0,0 +1,21 @@
+import {Stack} from "expo-router";
+
+export default function ListItemsLayout() {
+    return (
+        <Stack>
+            <Stack.Screen
+                name="[id]/[name]"
+                options={{
+                    headerShown: false
+                }}
+
+                // define a custom header for all screens in this route
+                // see [name].tsx, can also use useNavigation + useEffect
+                // options={({ route }) => ({
+                //     header: () => (<CustomHeader headerText={route.params?.name} searchText="item"/>),
+                // })}
+
+            />
+        </Stack>
+    )
+}
