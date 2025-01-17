baf14f58ba6940654774f7f7cb2c34eb631be02b
jiyu13 <jiyucoding@yahoo.com>
2025-01-17 10:35:27 -0500

    add headerStyle for CustomHeaderProps

--- types/type.d.ts
+++ types/type.d.ts
@@ -24,6 +24,7 @@ declare interface InputFieldProps extends TextInputProps {
 
 declare interface CustomHeaderProps {
     headerText: string;
+    headerStyle: string;
     searchText: string;
     children: React.ReactNode;
 }
