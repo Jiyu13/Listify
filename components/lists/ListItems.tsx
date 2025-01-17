7a313fa88678ea394abe4ec5bdfdb383a3d07bb8
jiyu13 <jiyucoding@yahoo.com>
2025-01-17 10:07:45 -0500

    convert item id to a string

--- components/lists/ListItems.tsx
+++ components/lists/ListItems.tsx
@@ -10,7 +10,7 @@ export default function ListItems({listItems}: {listItems: ListItem[]}) {
             <FlatList
                 data={listItems}
                 renderItem={({item}) => <ItemCard item={item}/>}
-                keyExtractor={(item) => item.id}
+                keyExtractor={(item) => item.id.toString()}   // FlatList requires keyExtractor to return a string
                 showsVerticalScrollIndicator={false}
                 className="rounded-2xl" //  mb-36
                 contentContainerStyle={{ paddingBottom: 110 }}  // applies styles to the inner content of the FlatList, ensure the last item is fully visible above the tab bar
