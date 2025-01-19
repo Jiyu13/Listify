fa090ca8acaab6ee6cff1b8677682f3fa2d6eec7
jiyu13 <jiyucoding@yahoo.com>
2025-01-18 20:45:33 -0500

    post new list by user id

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -9,12 +9,14 @@ import {ReactNativeModal} from "react-native-modal";
 import InputField from "@/components/InputField";
 import Lists from "@/components/lists/Lists";
 import {Context} from "@/components/Context";
+import api from "@/api";
+import {List} from "@/types/type";
 
 
 export default function HomePage() {
     const { isSignedIn } = useAuth()
     const {user} = useUser()
-    const {setAppUser, appUser} = useContext(Context)
+    const {setAppUser, appUser, userLists, setUserLists} = useContext(Context)
 
     const [showAddForm, setShowAddForm] = useState(false)
     const [newListData, setNewListData] = useState({name: ""})
@@ -22,9 +24,24 @@ export default function HomePage() {
     console.log("Home Page Loaded");
     // console.log("home-------------", appUser)
 
-    function handleAddOnPress() {
-       setShowAddForm(!showAddForm)
+    async function handleAddList() {
+        setShowAddForm(!showAddForm)
+        if (isSignedIn && appUser) {
+            const userId = appUser?.id
+            try {
+                const response = await api.post(`/lists/${userId}`, newListData)
+                const newList = response.data
+                // @ts-ignore
+                setUserLists((prev) => [...prev, newList])
+            } catch(error) {
+                console.error("Error adding item by list id:", error);
+
+            }
+        }
+
+        setNewListData({name: ""})
     }
+
     function handleInput(name: string, value: string) {
         setNewListData({...newListData, [name]: value})
     }
@@ -38,7 +55,7 @@ export default function HomePage() {
                 <View className="mb-4">
                     <View className="flex flex-row items-center justify-between h-20 ">
                         <Text className="text-3xl font-JakartaBold">Lists</Text>
-                        <TouchableOpacity onPress={handleAddOnPress}>
+                        <TouchableOpacity onPress={() => setShowAddForm(true)}>
                             <Ionicons name="add" size={32} color="#3e4e50" />
                         </TouchableOpacity>
                     </View>
@@ -60,7 +77,17 @@ export default function HomePage() {
 
 
                 {/* ====================== ADD A LIST FORM=========================*/}
-                <ReactNativeModal isVisible={showAddForm}>
+                <ReactNativeModal
+                    isVisible={showAddForm}
+                    backdropOpacity={0.3}
+                    backdropTransitionOutTiming={0} // Instantly remove the backdrop
+                    animationIn="slideInUp" // Controls how the modal appears
+                    animationOut="slideOutDown" // Controls how the modal disappears
+                    animationOutTiming={300} // Adjusts the duration of the closing animation
+                    onBackdropPress={() => setShowAddForm(false)}  // close modal if clicking outside <View>
+                    onBackButtonPress={() => setShowAddForm(false)} // for Android, handles back button press
+
+                >
                     <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
                         <Text className="text-2xl text-primary-900 font-JakartaBold mb-2 text-center">New List</Text>
                         <InputField
@@ -72,7 +99,7 @@ export default function HomePage() {
                         />
                         <CustomButton
                             title="Create"
-                            onPress={handleAddOnPress}
+                            onPress={handleAddList}
                             className="mt-5 bg-success-500"
                         />
                     </View>
