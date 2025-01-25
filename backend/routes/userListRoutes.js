bb409bcd4965124844b275f2a6e56be17e08ded0
jiyu13 <jiyucoding@yahoo.com>
2025-01-24 20:30:52 -0500

    add shared_code column to queries

--- backend/routes/userListRoutes.js
+++ backend/routes/userListRoutes.js
@@ -6,14 +6,14 @@ router.get('/:user_id', async (req, res) => {
     const {user_id} = req.params
 
     const user_lists = await pool.query(
-        'SELECT l.id, l.name, l.share, ' +
+        'SELECT l.id, l.name, l.share, l.shared_code, ' +
         'TO_CHAR(l.created_at, \'DD Mon YYYY HH12:MI:SS AM\') AS created_at, ' +
         'CAST(COUNT(li.id) AS INT) AS item_count ' +              // counts the number of items in the list_item table for each list
         'FROM lists l ' +
         'JOIN users_lists ul ON l.id = ul.list_id ' +
         'LEFT JOIN list_item li ON l.id = li.list_id ' +
         'WHERE ul.user_id = $1 ' +
-        'GROUP BY l.id, l.name, l.share, l.created_at ' + // Groups the results by the unique columns of the lists table
+        'GROUP BY l.id, l.name, l.share, l.shared_code, l.created_at ' + // Groups the results by the unique columns of the lists table
         'ORDER BY l.created_at DESC',
         [parseInt(user_id)]
     );
