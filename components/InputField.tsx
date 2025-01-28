923e7e4f1497d7caa7347b504786d899d649b58c
jiyu13 <jiyucoding@yahoo.com>
2025-01-27 20:47:00 -0500

    add borderRadius +text padding

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -27,12 +27,15 @@ export default function InputField({
                     <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle} text-primary-900`}>
                         {label}
                     </Text>
-                    <View className={`flex flex-row justify-start items-center relative bg-neutral-100 border border-neutral-100 focus:border-primary-500 ${containerStyle}`}>
+                    <View
+                        style={{borderRadius: 12}}
+                        className={`flex flex-row justify-start items-center relative bg-neutral-100 border border-neutral-100 focus:border-primary-500 ${containerStyle}`}
+                    >
                         {icon && (
                             <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`} />
                         )}
                         <TextInput
-                            className={`p-4 font-JakartaSemiBold text-[15px] flex-1 ${inputStyle} text-left`}
+                            className={`px-4 py-4 font-JakartaSemiBold text-lg flex-1 ${inputStyle} text-left`}
                             secureTextEntry={secureTextEntry}
                             {...props}
                         >
