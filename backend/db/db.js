f5cf0bcacdccfb9d3d7e380cc69e729ac632f4cd
jiyu13 <jiyucoding@yahoo.com>
2025-01-30 16:32:12 -0500

    loads env variables from correct env file

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,13 +3,20 @@
  */
 const { Pool } = require('pg')
 
+const dotenv = require("dotenv");
+const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development"
+dotenv.config({ path: envFile }); // Load the correct .env file
+
+// console.log("DB_HOST:", process.env.DB_HOST); // Debugging output
 
 const pool = new Pool ({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT,
+    connectionString: process.env.DB_HOST, // PostgreSQL URL
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 })
 
 module.exports =  pool;
