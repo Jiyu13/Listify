32849e50520b524162f9411dabb2e227804f1de3
jiyu13 <jiyucoding@yahoo.com>
2025-01-30 16:26:10 -0500

    commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -34,7 +34,7 @@ export default function AppLayout() {
   const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
   if (!publishableKey) {
     throw new Error(
-        'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+        'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env.development',
     )
   }
 
