69dcc468913a7a32dc2cb93a6c0451a43efc09f9
jiyu13 <jiyucoding@yahoo.com>
2025-02-01 21:59:25 -0500

    showing loading before lists are fetched

--- components/lists/Lists.tsx
+++ components/lists/Lists.tsx
@@ -13,6 +13,8 @@ export default function Lists(
     const { isSignedIn } = useAuth()
     const {setAppUser, appUser, userLists, setUserLists} = useContext(Context)
 
+    const [loadingLists, setLoadingLists] = useState(true)
+
     // const [userlists, setUserLists] = useState(null)
     const results = isSignedIn && searchInput === "" ?
         userLists
@@ -22,15 +24,21 @@ export default function Lists(
         }
     )
 
+    console.log("searchInput", searchInput)
+
     useEffect(() => {
         const fetchListsByUserId = async () => {
             if (isSignedIn && appUser) {
                 try {
                     const user_id = appUser?.id
                     const response = await api.get(`/ul/${user_id}`)
+                    console.log(response.data.length)
                     setUserLists(response.data)
+                    setLoadingLists(false)
                 } catch (error) {
                     console.error("Error fetching lists by user:", error);
+                } finally {
+                    setLoadingLists(false); // Stop loading after fetching
                 }
             }
         }
@@ -40,21 +48,28 @@ export default function Lists(
 
     return (
         <>
-            {userLists.length > 0 ?
-                <FlatList
-                    data={results}
-                    renderItem={({item}) => <ListCard list={item}/>}
-                    keyExtractor={(item) => item.id+item.name}
-                    showsVerticalScrollIndicator={false}
-                    className="rounded-2xl" //  mb-36
-                    contentContainerStyle={{ paddingBottom: 110 }}  // applies styles to the inner content of the FlatList, ensure the last item is fully visible above the tab bar
-                />
-                :
-                <View className='items-center justify-center' style={{ height: "80%"}}>
-                    <Text className='text-customText-logo font-Jakarta text-lg'>
-                        You don't have any list. Add one!
-                    </Text>
-                </View>
+            {
+                loadingLists ? (
+                    <View className='items-center justify-center' style={{ height: "80%"}}>
+                        <Text className='text-customText-logo font-Jakarta text-lg'>
+                            Loading your lists...
+                        </Text>
+                    </View>
+                ) : userLists.length > 0 ?
+                    <FlatList
+                        data={results}
+                        renderItem={({item}) => <ListCard list={item}/>}
+                        keyExtractor={(item) => item.id+item.name}
+                        showsVerticalScrollIndicator={false}
+                        className="rounded-2xl" //  mb-36
+                        contentContainerStyle={{ paddingBottom: 110 }}  // applies styles to the inner content of the FlatList, ensure the last item is fully visible above the tab bar
+                    />
+                    :
+                    <View className='items-center justify-center' style={{ height: "80%"}}>
+                        <Text className='text-customText-logo font-Jakarta text-lg'>
+                            You don't have any list. Add one!
+                        </Text>
+                    </View>
             }
         </>
 
