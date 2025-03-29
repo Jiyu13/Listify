e9141e1c19a134ff0c58b7cb48080173815e8480
jiyu13 <jiyucoding@yahoo.com>
2025-03-28 20:23:07 -0400

    check mode + render different base url

--- api.ts
+++ api.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 
-const baseURL = process.env.EXPO_PUBLIC_BASE_URL
+let baseURL
+if (process.env.NODE_ENV == "development") {
+    baseURL = "http://192.168.1.168:5000/api/v1/listify"
+} else {
+    baseURL = process.env.EXPO_PUBLIC_BASE_URL
+}
+// const baseURL = process.env.EXPO_PUBLIC_BASE_URL
+console.log("baseURL", baseURL)
 
 const api = axios.create({
     baseURL: baseURL,
@@ -8,6 +15,5 @@ const api = axios.create({
         "Content-Type": "application/json",
     },
 })
-
 export default api
 
